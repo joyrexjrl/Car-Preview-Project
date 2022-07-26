6b1425acc5be03a06e7ac716c8c267e72ea818f5
joyrexjrl <ricklozano@gmail.com>
2022-07-26 13:29:16 -0700

    added bot model, added resizing
    added resizing functionality so the models stay within the middle of the viewport depending on screen size. added secondary model to try and get it attached to a dev window

---
+++ modelLoader.js
@@ -0,0 +1,78 @@
+let scene, camera, renderer;
+
+windowed = document.getElementById('windowed');
+
+function init(){
+    
+
+    scene = new THREE.Scene();
+    scene.background = new THREE.Color(0xdddddd);
+
+    camera = new THREE.PerspectiveCamera(40,window.innerWidth / window.innerHeight,1,5000);
+    camera.rotation.y = 45/180*Math.PI;
+    camera.position.x = 800;
+    camera.position.y = 100;
+    camera.position.z = 1000;
+
+    window.addEventListener('resize', function(){
+        var WIDTH = window.innerWidth,
+            HEIGHT = window.innerHeight;
+        renderer.setSize(WIDTH,HEIGHT);
+        camera.aspect = WIDTH / HEIGHT;
+        camera.updateProjectionMatrix();
+        render();
+    });
+
+    renderer = new THREE.WebGLRenderer({antialias:true});
+    renderer.setSize(window.innerWidth,window.innerHeight);
+    document.body.appendChild(renderer.domElement);
+
+    controls = new THREE.OrbitControls(camera, renderer.domElement);
+    controls.addEventListener('change', render);
+
+    hlight = new THREE.AmbientLight(0x404040,100);
+    scene.add(hlight);
+
+    directionalLight = new THREE.DirectionalLight(0xc4c4c4,100);
+    directionalLight.position.set(0,1,0);
+    directionalLight.castShadow = true;
+    scene.add(directionalLight);
+
+    light = new THREE.PointLight(0xc4c4c4,10);
+    light.position.set(0,300,500);
+    scene.add(light);
+
+    light2 = new THREE.PointLight(0xc4c4c4,10);
+    light2.position.set(500,100,0);
+    scene.add(light2);
+
+    light3 = new THREE.PointLight(0xc4c4c4,10);
+    light3.position.set(0,100,-500);
+    scene.add(light3);
+
+    light4 = new THREE.PointLight(0xc4c4c4,10);
+    light4.position.set(-500,300,0);
+    scene.add(light4);                        
+
+    let loader = new THREE.GLTFLoader();
+    loader.load("car/scene.gltf", function(gltf){
+        car = gltf.scene.children[0];
+        car.scale.set(0.5,0.5,0.5);
+        scene.add(gltf.scene);
+        render();
+    });
+
+    loader.load("bot/scene.gltf", function(gltf){        
+        bot = gltf.scene.children[0];
+        bot.scale.set(1,1,1);
+        bot.position.set(0,300,0);
+        scene.add(gltf.scene);
+        render();
+    });
+}
+
+function render(){
+    renderer.render(scene, camera);
+}
+
+init();
